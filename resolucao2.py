dc302b9b6c611b2170dc4088284e9f88b4b6c1b7
Júlia Borges <62912240+juliaborgess18@users.noreply.github.com>
2022-10-03 21:06:17 -0300

    Delete resolucao2.py

--- resolucao2.py
+++
@@ -1,9 +0,0 @@
-valores = input().split() #Utilizando a função split para dividir o conteúdo da variável de acordo com as condições especificadas 
-
-# TODO:  Calcule a média de cachorros quentes consumidas por participante e imprima o valor com DUAS casas decimais.
-h = int(valores[0])
-p = int(valores[1])
-
-media = h/p 
-
-print(f"{media: .2f}")
